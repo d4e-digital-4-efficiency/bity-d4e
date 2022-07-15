d4e353cec404cd71d69e27fb38c998d5d6c8ae62
d4e-ch <52782157+d4e-ch@users.noreply.github.com>
2022-07-15 17:00:38 +0200

    + d4e_currency_precision

---
+++ d4e_currency_precision/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "D4E Currency Precision",
+    'summary': """Digital4Efficiency - Currency Precision""",
+    'description': """D4E - Currency Precision""",
+    'category': 'Currency',
+    'version': '14.0.1.0',
+    'depends': [
+        'product',
+        'account',
+    ],
+    'data': [
+        'data/decimal_precision.xml',
+        'data/res_currency.xml',
+    ],
+}
